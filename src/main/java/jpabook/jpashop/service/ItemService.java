43b2dba2d6ed15156ccb874a4a6eb9cb1c25cae2
Somin Park <ps4708@naver.com>
2023-10-05 22:43:04 +0900

    feat: add web service

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -18,6 +18,17 @@ public class ItemService {
     public void saveItem(Item item) {
         itemRepository.save(item);
     }
+
+    @Transactional
+    public void updateItem(Long id, String name, int price, int stockQuantity)
+    {
+        Item item = itemRepository.findOne(id);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
+        //직접 set을 호출하는 것보다 item.change 처럼 entity 내에 변경 메서드를 만다는 것이 좋음
+    }
+
     public List<Item> findItems() {
         return itemRepository.findAll();
     }
--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -8,9 +8,12 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * 상품 주문
@@ -70,6 +73,11 @@ public class OrderService {
         //주문 취소
         order.cancel();
         return order.getId();
+    }
+
+    public List<Order> findOrder(OrderSearch orderSearch) {
+        List<Order> orders = orderRepository.findAllByCriteria(orderSearch);
+        return orders;
     }
 
     //검색

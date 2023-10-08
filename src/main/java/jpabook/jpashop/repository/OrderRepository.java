c6b4a7ae64f460c321d7d03ba71a2a6a193be765
Somin Park <ps4708@naver.com>
2023-10-08 20:32:12 +0900

    feat: add x to one api

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -86,5 +86,17 @@ public class OrderRepository {
         return query.getResultList();
     }
 
+    public List<Order> findAll() {
+        return em.createQuery("select o from Order o", Order.class)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                        "select o from Order o" +
+                                " join fetch o.member m" +
+                                " join fetch o.delivery d", Order.class)
+                .getResultList();
+    }
 
 }

b969d2504fd1e6b2a796f0845e5d201ae972d7b9
Somin Park <ps4708@naver.com>
2023-12-28 14:43:25 +0900

    feat: add x to many

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -93,4 +93,18 @@ public class OrderQueryRepository {
         return orderItems.stream()
                 .collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
     }
+
+    /**
+     * Query: 1번
+     * 모든 걸 한번에 다 조인
+     */
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery( "select new jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                        " from Order o" +
+                                " join o.member m" +
+                                " join o.delivery d" +
+                                " join o.orderItems oi" +
+                                " join oi.item i", OrderFlatDto.class)
+                .getResultList();
+    }
 }
